aa4281104e9e3d9dedd4479ca10bf170d42e8053
Anwitha Arbi <38724750+Anwitha2806@users.noreply.github.com>
2024-12-03 23:37:31 -0800

    first commit

--- Flight-Reservation-System-Backend_Updated/src/main/java/com/flightreservation/FlightReservationSystemApplication.java
+++ Flight-Reservation-System-Backend_Updated/src/main/java/com/flightreservation/FlightReservationSystemApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class FlightReservationSystemApplication {
-
+	// main function
 	public static void main(String[] args) {
 		SpringApplication.run(FlightReservationSystemApplication.class, args);
 	}
